5e4e0318977c925e29a7c963a6981dd51341b415
Dell Home <pon.felipon@gmail.com>
2021-01-07 07:06:03 -0700

    codigo add sense, y cajas de input

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def summoner():
         user_champs = grab_champs(user['summoner'], user['network'])
         most_used = grab_most_used(user_champs, 10)
         info, top_three, worst_three  = find_counters(user['enemy'], user['line'], most_used)
-        print(user)
         return render_template('index.html', user=user, info=info, top_three=top_three, worst_three=worst_three)
     elif request.method == 'GET':
         # return '<a href="http://aws.syslinux.xyz:5000">"http://aws.syslinux.xyz:5000"</a>'
